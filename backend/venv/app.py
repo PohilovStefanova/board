1d7a4c3aeb7685a2fbc94f8ffc30132a9adde431
oceanm7n <alex@10.0.0.4>
2019-05-31 23:29:22 +0300

    MongoDB + GET request

--- backend/venv/app.py
+++ backend/venv/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
 
-app = Flask(__name__)
+# Приложение 
+app = Flask(__name__);
 
+# Конфигурация базы данных 
+app.config['MONGO_DBNAME'] = 'threads'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
+
+# test
 @app.route('/')
 def hello_world():
     return 'Hello world'
+
+# GET метод по URL '/api/threads/' для получения всех тредов.
+@app.route('/api/threads', methods=['GET'])
+def get_threads():
+    threads = mongo.db.threads
+    output = []
+    # Из каждого найденного документа пихаем в массив output необходимые поля
+    for doc in threads.find():
+        output.append({'_id': doc['_id'], 'created_on':doc['created_on'], 'updated_on':doc['updated_on'], 'text': doc['text'], 'replies': doc['replies']})
+    return JSONEncoder().encode(output)
