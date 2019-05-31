6a45f4f6c3e458d9324e225e943152b5a09c47b7
PohiloveEvgeniy <alex@10.0.0.4>
2019-05-31 23:39:14 +0300

    import fix + pymongo reinstalled

--- backend/venv/app.py
+++ backend/venv/app.py
@@ -1,4 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo, ObjectId
+import json
+from bson import ObjectId
+import uuid
+import datetime
 
 # Приложение 
 app = Flask(__name__);
