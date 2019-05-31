0579f746beb1b7618482a4185c56a8221afea9be
PohiloveEvgeniy <alex@10.0.0.4>
2019-05-31 23:36:27 +0300

    output fix

--- backend/venv/app.py
+++ backend/venv/app.py
@@ -5,13 +5,14 @@ app = Flask(__name__);
 
 # Конфигурация базы данных 
 app.config['MONGO_DBNAME'] = 'threads'
+# локальная база данных, нужен mongodb
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
 
 # test
 @app.route('/')
 def hello_world():
-    return 'Hello world'
+    return 'test test test'
 
 # GET метод по URL '/api/threads/' для получения всех тредов.
 @app.route('/api/threads', methods=['GET'])
@@ -20,5 +21,5 @@ def get_threads():
     output = []
     # Из каждого найденного документа пихаем в массив output необходимые поля
     for doc in threads.find():
-        output.append({'_id': doc['_id'], 'created_on':doc['created_on'], 'updated_on':doc['updated_on'], 'text': doc['text'], 'replies': doc['replies']})
+        output.append({'_id': doc['_id'], 'text': doc['text'], 'created_on':doc['created_on'], 'updated_on':doc['updated_on'], 'replies': doc['replies']})
     return JSONEncoder().encode(output)
